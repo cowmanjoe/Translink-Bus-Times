bfbe9ca53936e0d82f6889c0a654698738af4eb5
Cowan Cumming <Cowan Cumming>
2017-01-14 17:37:41 -0800

    Implemented DeleteableText class
    Haven't actually tested the class

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/11/00cc8512abda00161d35dba80c5c9970
@@ -0,0 +1,8 @@
+package translink;
+
+import javafx.scene.Group;
+
+public class DeleteableText extends Group{
+	
+	//TODO: Implement. DeleteableText will be a label with a button directly to its side that removes the DeleteableText
+}
--- Translink/src/translink/DeleteableText.java
+++ Translink/src/translink/DeleteableText.java
@@ -1,8 +1,28 @@
 package translink;
 
 import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class DeleteableText extends Group{
 	
 	//TODO: Implement. DeleteableText will be a label with a button directly to its side that removes the DeleteableText
+	
+	private Label text; 
+	private Button deleteButton; 
+	
+	public DeleteableText() { 
+		text = new Label(); 
+		deleteButton = new Button(); 
+	}
+	
+	public DeleteableText(String text) { 
+		
+	}
+	
+	public void setText(String text) { 
+		
+	}
+	
+	
 }
